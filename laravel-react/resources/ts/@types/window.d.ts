a477382a0e9385a7876bbe39cccae39c6b3d9ce8
hiroto <suizlet@gmail.com>
2020-07-29 21:04:06 +0900

    inject typescript project

---
+++ laravel-react/resources/ts/@types/window.d.ts
@@ -0,0 +1,7 @@
+interface Window {
+    _     : any;
+    Popper: any;
+    $     : any;
+    jQuery: any;
+    axios : any;
+}
--- laravel-react/resources/views/welcome.blade.php
+++ laravel-react/resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
     </div>
 
     <!-- JavaScript -->
-    <script src="{{ asset('js/app.js')}}"></script>
+    <script src="{{ mix('js/app.js')}}"></script>
 </body>
 
 </html>
